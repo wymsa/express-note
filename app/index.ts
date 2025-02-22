604f98c38c630b1d067e0775ad3d431ac2a18e1e
wymsa <wymsamail@gmail.com>
2025-02-22 11:29:26 +0200

    added router initialization

--- app/index.ts
+++ app/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
+import mainRouter from "./router";
 
 const application = express();
+application.use("/api", mainRouter);
 
 application.listen(3001, () => {
   console.log("Server is running");
